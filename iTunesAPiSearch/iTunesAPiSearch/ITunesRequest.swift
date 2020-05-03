2b43622e4a6e3bab600a32c2549d656da435b0ec
gitpiotrek <piotr.w3w@gmail.com>
2020-05-03 23:08:41 +0200

    fix location request and model

--- iTunesAPiSearch/iTunesAPiSearch/ITunesRequest.swift
+++ iTunesAPiSearch/iTunesAPiSearch/ITunesRequest.swift
@@ -8,18 +8,38 @@ enum ITunesError: Error {
 
 struct ITunesRequest {
     let resourceURL: URL
+
     
     init(name: String) {
 
-        
                 let resourceString = "https://itunes.apple.com/search?term=\(name)&limit=5&mediaType=music"
 
-        let replaced = resourceString.replacingOccurrences(of: " ", with: "+") // SPACJE TRZEBA NA + ZAMIENIC W LINKU
+        let replaced = resourceString.replacingOccurrences(of: " ", with: "+")
 
                 guard let resourceURL = URL(string: replaced) else {fatalError()}
                 
                 self.resourceURL = resourceURL
-        // TRZEBA JAKIS DATA MODEL ZROBIC
+            }
+            
+            
+            // API Request - getter
+            func getITunes (completion: @escaping(Result<[App], ITunesError>) -> Void) {
+                // happends async with main thread
+                  URLSession.shared.dataTask(with: resourceURL) { (data, response, error) in
+                    guard let data = data else { return }
+                    
+                    do {
+               
+                
+                                let decodedResponse = try JSONDecoder().decode(ITunesResponse.self, from: data)
+                        let app = decodedResponse.results.unsafelyUnwrapped
+                        completion(.success(app))
+                    } catch {
+                        completion(.failure(.cannotProcessData))
+                    }
+                    
+                }
+                .resume()
             }
                
         }
